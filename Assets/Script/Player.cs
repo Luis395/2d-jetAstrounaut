eab703cf27f8c6197d00d766aefdd962053e3035
Luis395 <lfvmp1@gmail.com>
2020-04-21 19:00:24 -0300

    v0.1.1

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -10,14 +10,14 @@ public class Player : MonoBehaviour
     bool isGrounded;
     bool doubleJump;
     private Animator anim;
-    // Start is called before the first frame update
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         Move();
