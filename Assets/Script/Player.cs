98def860f5f7ebf0fc63ddf0de41d348a881203d
Luis395 <lfvmp1@gmail.com>
2020-04-21 18:53:38 -0300

    v0.1

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    private Rigidbody2D rb;
+    bool isGrounded;
+    bool doubleJump;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+    void Move()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(movement * Speed, rb.velocity.y);
+        if(movement > 0) 
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        if (movement < 0)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0180f, 0f);
+        }
+        if (movement == 0)
+        {
+            anim.SetBool("walk", false);
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if(isGrounded) 
+            {
+                rb.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+                anim.SetBool("jump", true);
+            }
+            else 
+            {
+                if (doubleJump) 
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, 0f);
+                    rb.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+
+            }
+           
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isGrounded = true;
+            anim.SetBool("jump", false);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isGrounded = false;
+        }
+    }
+}
